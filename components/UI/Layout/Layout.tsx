c0d2e891027907fff9647b4c47607194e20dd218
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-12-13 20:18:02 +0530

    update: layout data

--- components/UI/Layout/Layout.tsx
+++ components/UI/Layout/Layout.tsx
@@ -3,28 +3,26 @@ import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 
 import DATA from '../../../data.json';
+import Header from '../Header/Header';
 
 type Props = {
     children: React.ReactNode;
-    name: string;
 };
 
-const Layout = ({ name, children }: Props): JSX.Element => {
+const Layout = ({ children }: Props): JSX.Element => {
     return (
         <>
             <Head>
-                <title>{`Resume | ${name}`}</title>
+                <title>{`Resume | ${DATA.name}`}</title>
             </Head>
-
             <Navbar download={DATA.pdf} link={{ url: DATA.url, text: 'Portfolio' }} />
-
+            <Header />
             <main>{children}</main>
-
             <Footer
                 content={
                     <>
                         <i className='fa-regular fa-copyright'></i>
-                        &nbsp;{name} 2022
+                        &nbsp;{DATA.name} 2022
                     </>
                 }
             />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import DATA from '../data.json';
 
 import Layout from '../components/UI/Layout/Layout';
 
 export default ({ Component, pageProps }: AppProps) => {
     return (
-        <Layout name={DATA.name}>
+        <Layout>
             <Component {...pageProps} />
         </Layout>
     );
