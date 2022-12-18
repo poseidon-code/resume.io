b3703a88770cc30f95003c0be7128f95bd811b7a
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2022-12-18 20:37:34 +0530

    add: main container styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
 
+import styles from '../styles/Home.module.scss';
+
 const Home: NextPage = () => {
-    return <>Home</>;
+    return (
+        <>
+            <section className={styles.Left}></section>
+            <section className={styles.Right}></section>
+        </>
+    );
 };
 
 export default Home;
