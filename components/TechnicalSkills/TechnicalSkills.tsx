32ec9ae0607b252278191aca525b11075ad00ef3
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2023-08-08 10:00:00 +0530

    update: technical skills list item keys

--- components/TechnicalSkills/TechnicalSkills.tsx
+++ components/TechnicalSkills/TechnicalSkills.tsx
@@ -1,21 +1,21 @@
-import styles from './TechnicalSkills.module.scss';
+import styles from "./TechnicalSkills.module.scss";
 
-import { keyCapitalise } from '../../utilities';
-import DATA from '../../data.json';
+import { keyCapitalise } from "../../utilities";
+import DATA from "../../data.json";
 
 const TechnicalSkills = (): JSX.Element => {
     return (
         <article className={styles.TechnicalSkills}>
             <h2 className='title'>TECHNICAL SKILLS</h2>
-            {Object.keys(DATA.technical_skills).map((key: string) => {
+            {Object.keys(DATA.technical_skills).map((key: string, i: number) => {
                 return (
                     <>
-                        <h4 key={`${key}`}>{keyCapitalise(key)}</h4>
-                        {DATA.technical_skills[key as keyof typeof DATA.technical_skills].map((ts: string, i: number) =>
-                            DATA.technical_skills[key as keyof typeof DATA.technical_skills].length !== i + 1 ? (
-                                <span key={`${ts}-${i}`}> {ts} &bull;</span>
+                        <h4 key={`ts-group-${key}-${i}`}>{keyCapitalise(key)}</h4>
+                        {DATA.technical_skills[key as keyof typeof DATA.technical_skills].map((ts: string, j: number) =>
+                            DATA.technical_skills[key as keyof typeof DATA.technical_skills].length !== j + 1 ? (
+                                <span key={`ts-item-${key}-${j}`}> {ts} &bull;</span>
                             ) : (
-                                <span> {ts}</span>
+                                <span key={`ts-item-${key}-${j}`}> {ts}</span>
                             )
                         )}
                     </>
