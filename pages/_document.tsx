8e45b1099d9f35fe596d01a4f8161d2403fdee4f
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2023-08-10 10:00:00 +0530

    update: supacons font icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
     return (
@@ -14,7 +14,10 @@ const Document = () => {
                     href='https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
                     rel='stylesheet'
                 />
-                <link rel='stylesheet' href='https://poseidon-code.github.io/supacons/dist/supacons.regular.css' />
+                <link
+                    rel='stylesheet'
+                    href='https://poseidon-code.github.io/supacons/dist/classic/supacons.regular.css'
+                />
             </Head>
             <body>
                 <Main />
