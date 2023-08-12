96e5d73e6d61d8506d3fb333b72093390bf4e7f0
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2023-08-12 10:00:00 +0530

    add: socials

---
+++ components/Socials/Socials.tsx
@@ -0,0 +1,23 @@
+import styles from "./Socials.module.scss";
+
+import DATA from "../../data.json";
+
+const Socials = (): JSX.Element => {
+    return (
+        <article className={styles.Socials}>
+            <h2 className='title'>SOCIAL LINKS</h2>
+            <ul>
+                {DATA.socials.map((social: { link: string; text: string }, i: number) => (
+                    <li>
+                        <a key={`social-${i}`} href={social.link} target='_blank' rel='noopener noreferrer'>
+                            <i className='fa-regular fa-link-horizontal'></i>
+                            {social.text}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </article>
+    );
+};
+
+export default Socials;
