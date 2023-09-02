08686eb1793be268c8763fef844d2a09b2b65135
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2023-09-02 10:00:00 +0530

    fix: list keys for Achievements

--- components/Achievements/Achievements.tsx
+++ components/Achievements/Achievements.tsx
@@ -8,8 +8,8 @@ const Achievements = (): JSX.Element => {
             <h2 className='title'>ACHIEVEMENTS</h2>
             <ul>
                 {DATA.achievements.map((achievement: { link: string; text: string }, i: number) => (
-                    <li>
-                        <a key={`achievement-${i}`} href={achievement.link} target='_blank' rel='noopener noreferrer'>
+                    <li key={`achievement-${i}`}>
+                        <a href={achievement.link} target='_blank' rel='noopener noreferrer'>
                             <i className='fa-regular fa-medal'></i>
                         </a>
                         <span> {achievement.text}</span>
